2ac4696039cafb45eb3fef25ad6f73f86363097d
ck <ck@163.com>
2018-11-21 19:49:55 +0800

    新闻界面基本布局

--- src/main.js
+++ src/main.js
@@ -2,22 +2,26 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import moment from 'moment'
 
 // 导入 vue-resourse
 import VueResource from "vue-resource"
 
 // 注册
 Vue.use(VueResource)
+// 配置vue-resource请求根域名
+Vue.http.options.root="http://www.lovegf.cn:8899/"
 
 Vue.config.productionTip = false
 // 导入mint-ui组件
 // import "mint-ui/lib/style.css"
-import {Header,Swipe, SwipeItem} from 'mint-ui'
+import {Header,Swipe, SwipeItem,Button} from 'mint-ui'
 
 // 注册组件
 Vue.component(Header.name, Header)
 Vue.component(Swipe.name, Swipe);
 Vue.component(SwipeItem.name, SwipeItem);
+Vue.component(Button.name,Button)
 
 // 导入mui样式
 import "./lib/mui/css/mui.css"
@@ -25,6 +29,11 @@ import  "./lib/mui/css/icons-extra.css"
 
 // 初始化样式
 import "./css/common.less";
+
+// 定义全局过滤器
+Vue.filter('dataFormat',function (dataStr,pattern="YYYY-MM-DD HH:mm:ss") {
+  return moment(dataStr).format(pattern)
+  })
 
 /* eslint-disable no-new */
 
--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -12,9 +12,11 @@
 
         <!-- 九宫格 -->
             <ul class="mui-table-view mui-grid-view mui-grid-9">
-		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
+		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3">
+                  <router-link to="/home/newsList">
 		                   <img src="../../images/menu1.png" alt="">
-		                    <div class="mui-media-body">新闻资讯</div></a></li>
+		                    <div class="mui-media-body">新闻资讯</div>
+                   </router-link></li>
 		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
 		                     <img src="../../images/menu2.png" alt="">
 		                    <div class="mui-media-body">图片分享</div></a></li>
@@ -30,7 +32,6 @@
 		            <li class="mui-table-view-cell mui-media mui-col-xs-4 mui-col-sm-3"><a href="#">
 		                  <img src="../../images/menu6.png" alt="">
 		                    <div class="mui-media-body">联系我们</div></a></li>
-		          
 		        </ul> 
     </div>
 </template>
@@ -91,14 +92,14 @@ export default {
   .mui-grid-view {
     background-color: #fff;
     border: none;
-    .mui-table-view-cell{
-        border: none;
-        img{
-            width: 60px;
-        }
-        .mui-media-body{
-            font-size: 13px;
-        }
+    .mui-table-view-cell {
+      border: none;
+      img {
+        width: 60px;
+      }
+      .mui-media-body {
+        font-size: 13px;
+      }
     }
   }
 }
